aad651464df5ce10cf48a8557cbb36162111fc93
kiieeaa <kiyaadinda5@gmail.com>
2026-03-31 10:46:09 +0700

    Validasi panjang array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -11,6 +11,16 @@ void input()
         cout << "Masukkan Panjang element array : ";
         cin >> n;
 
+         if (n <= 20)
+        {
+            break; // keluar dari perulangan jika valid
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
     
     
     
